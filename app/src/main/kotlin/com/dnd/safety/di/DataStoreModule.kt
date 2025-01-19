03e777b264e50e1776b31fac9ee6ecb6d34f4197
jhbae12346 <jhbae@logishm.com>
2025-01-19 21:44:55 +0900

    feat: datastore 코드 추가 (#1)

---
+++ app/src/main/kotlin/com/dnd/safety/di/DataStoreModule.kt
@@ -0,0 +1,30 @@
+package com.dnd.safety.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import com.dnd.safety.data.datastore.model.UserPreferences
+import com.dnd.safety.data.datastore.serializer.UserPreferencesSerializer
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    private const val USER_PREFERENCES_DATASTORE_NAME = "USER_PREFERENCES"
+
+    private val Context.userPreferenceDataStore by dataStore(
+        USER_PREFERENCES_DATASTORE_NAME, UserPreferencesSerializer
+    )
+
+    @Provides
+    @Singleton
+    fun provideUserPreferenceDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<UserPreferences> = context.userPreferenceDataStore
+}
