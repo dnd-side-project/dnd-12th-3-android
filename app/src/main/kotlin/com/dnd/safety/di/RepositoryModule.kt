71706770c1284b0c7bbbef97c138e7d63d9f6128
jhbae12346 <jhbae@logishm.com>
2025-01-24 21:18:55 +0900

    fix: @Binds로 인터페이스 구현체 주입
    이미 dao와 api가 provide되고 있기 때문에 기존 방식대로 수동으로 주입하지 않아도 됩니다

--- app/src/main/kotlin/com/dnd/safety/di/RepositoryModule.kt
+++ app/src/main/kotlin/com/dnd/safety/di/RepositoryModule.kt
@@ -1,22 +1,20 @@
 package com.dnd.safety.di
 
-import com.dnd.safety.data.local.dao.SampleDao
-import com.dnd.safety.data.remote.api.SampleApi
 import com.dnd.safety.data.repository.SampleRepositoryImpl
 import com.dnd.safety.domain.repository.SampleRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
-    @Provides
+abstract class RepositoryModule {
+
+    @Binds
     @Singleton
-    fun provideSampleRepository(
-        api: SampleApi,
-        dao: SampleDao,
-    ): SampleRepository = SampleRepositoryImpl(api, dao)
+    abstract fun provideSampleRepository(
+        sampleRepositoryImpl: SampleRepositoryImpl
+    ): SampleRepository
 }
