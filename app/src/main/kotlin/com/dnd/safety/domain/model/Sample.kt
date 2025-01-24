8ecc2f14485c231e221c4401b8ae8d3b3734c943
jhbae12346 <jhbae@logishm.com>
2025-01-24 21:17:52 +0900

    fix: Parcelable 제거
    Parcelable에 대한 의존성은 아직 필요성을 못느껴 제거하였습니다

--- app/src/main/kotlin/com/dnd/safety/domain/model/Sample.kt
+++ app/src/main/kotlin/com/dnd/safety/domain/model/Sample.kt
@@ -1,17 +1,14 @@
 package com.dnd.safety.domain.model
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
-@Parcelize
 @Serializable
 data class Sample(
     val id: Int,
     val name: String,
     val description: String,
     val createdAt: String,
-): Parcelable {
+) {
     val displayName: String
         get() = "[$id] $name"
 }
