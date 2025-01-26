ec6e5d416b07f001db06121e281400ceaf9bc6bf
jhbae12346 <jhbae@logishm.com>
2025-01-26 16:29:18 +0900

    feat: 커스텀 네비게이션 확장함수에 대한 주석 추가 (#2)

--- app/src/main/kotlin/com/dnd/safety/presentation/navigation/utils/ComposableType.kt
+++ app/src/main/kotlin/com/dnd/safety/presentation/navigation/utils/ComposableType.kt
@@ -11,6 +11,18 @@ import androidx.navigation.NavDeepLink
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 
+/**
+ * [NavGraphBuilder]에 [composable]을 추가하는 확장 함수입니다.
+ * 이 함수는 커스텀한 매개변수를 전달할 필요가 있을 경우에 사용할 수 있습니다.
+ * [Type]은 매개변수의 타입을 나타냅니다.
+ *
+ * 일반적인 상황에서는 [composable]을 사용하면 되지만, 매개변수를 전달해야할 경우에 사용하면 됩니다.
+ * ```
+ * composableType<Route.Detail, DetailInfo> { backStackEntry ->
+ *     val id = backStackEntry.toRoute<DetailInfo>().id
+ * }
+ * ```
+ */
 inline fun <reified T : Any, reified Type : Any> NavGraphBuilder.composableType(
     deepLinks: List<NavDeepLink> = emptyList(),
     noinline enterTransition:
