c2f35605cd9f0eef31cb5d982b7e6f98a8fb569b
jhbae12346 <jhbae@logishm.com>
2025-01-29 13:30:50 +0900

    feat: 사용자 위치 기능 추가 (ECS-11)

---
+++ app/src/main/kotlin/com/dnd/safety/location/LocationServiceImpl.kt
@@ -0,0 +1,56 @@
+package com.dnd.safety.location
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.os.Looper
+import androidx.annotation.RequiresApi
+import com.dnd.safety.utils.hasLocationPermission
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
+class LocationServiceImpl @Inject constructor(
+    private val context: Context,
+    private val locationClient: FusedLocationProviderClient
+) : LocationService {
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun requestLocationUpdates(): Flow<LatLng?> = callbackFlow {
+        if (!context.hasLocationPermission()) {
+            trySend(null)
+            return@callbackFlow
+        }
+
+        val request = LocationRequest.Builder(10000L)
+            .setIntervalMillis(10000L)
+            .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+            .build()
+
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.locations.lastOrNull()?.let {
+                    trySend(LatLng(it.latitude, it.longitude))
+                }
+            }
+        }
+
+        locationClient.requestLocationUpdates(
+            request,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+
+        awaitClose {
+            locationClient.removeLocationUpdates(locationCallback)
+        }
+    }
+}
