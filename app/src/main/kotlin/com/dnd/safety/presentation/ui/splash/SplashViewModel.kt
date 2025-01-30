08555ab892c190a3f469357bc43b8348f5451b4a
jhbae12346 <jhbae@logishm.com>
2025-01-30 10:40:24 +0900

    fix: Splash 불필요 코드 제거 (ECS-11)

--- app/src/main/kotlin/com/dnd/safety/presentation/ui/splash/SplashViewModel.kt
+++ app/src/main/kotlin/com/dnd/safety/presentation/ui/splash/SplashViewModel.kt
@@ -2,11 +2,8 @@ package com.dnd.safety.presentation.ui.splash
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.dnd.safety.location.LocationService
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
--- app/src/main/kotlin/com/dnd/safety/presentation/ui/splash/navigation/SplashNavigation.kt
+++ app/src/main/kotlin/com/dnd/safety/presentation/ui/splash/navigation/SplashNavigation.kt
@@ -1,15 +1,9 @@
 package com.dnd.safety.presentation.ui.splash.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.dnd.safety.presentation.navigation.Route
 import com.dnd.safety.presentation.ui.splash.SplashRoute
-
-fun NavController.navigateToSplash(navOptions: NavOptions) {
-    navigate(Route.Splash, navOptions)
-}
 
 fun NavGraphBuilder.splashNavGraph(
     onPermissionAllowed: () -> Unit,
