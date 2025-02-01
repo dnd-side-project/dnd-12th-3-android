ba6aee17a24e8f883c87d31b5fae0c9a3101ad21
jhbae12346 <jhbae@logishm.com>
2025-02-01 15:50:23 +0900

    feat: enableEdgeToEdge 설정

--- app/src/main/kotlin/com/dnd/safety/presentation/MainActivity.kt
+++ app/src/main/kotlin/com/dnd/safety/presentation/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -19,6 +20,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
             val navigator: MainNavigator = rememberMainNavigator()
             val coroutineScope = rememberCoroutineScope()
