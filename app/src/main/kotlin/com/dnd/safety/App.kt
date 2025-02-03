924f6663c079d543d8ed241e6988d4f549d5d1b7
StopStone <wltjr6432@naver.com>
2025-02-03 21:07:04 +0900

    Chore: 카카오 API 초기화

--- app/src/main/kotlin/com/dnd/safety/App.kt
+++ app/src/main/kotlin/com/dnd/safety/App.kt
@@ -1,8 +1,13 @@
 package com.dnd.safety
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+    }
 }
