59d2a897ccffca25e7eddc22a1be25d43df4b0f2
StopStone <wltjr6432@naver.com>
2025-02-04 21:11:38 +0900

    Feat: 구글 OAuth2 소셜 로그인 추가 - DataStore 키 관리 미구현 - 백엔드 서버 통신 미구현

--- app/src/main/kotlin/com/dnd/safety/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/kotlin/com/dnd/safety/data/repository/AuthRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.dnd.safety.data.repository
 
+import com.dnd.safety.data.model.request.GoogleLogInRequest
+import com.dnd.safety.data.remote.api.GoogleAuthService
 import com.dnd.safety.domain.datasource.KakaoLoginDataSource
 import com.dnd.safety.domain.repository.AuthRepository
 import com.skydoves.sandwich.ApiResponse
@@ -8,6 +10,7 @@ import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val kakaoLoginDataSource: KakaoLoginDataSource,
+    private val googleAuthService: GoogleAuthService,
 ) : AuthRepository {
     override suspend fun loginWithKakao(): Result<String> {
         return when (val response = kakaoLoginDataSource.login()) {
@@ -21,4 +24,17 @@ class AuthRepositoryImpl @Inject constructor(
         }
     }
 
+    override suspend fun signInWithGoogle(idToken: String): Result<String> {
+        return when (val response = googleAuthService.googleSignIn(GoogleLogInRequest(idToken))) {
+            is ApiResponse.Success -> {
+                Result.success(response.data.accessToken)
+            }
+            is ApiResponse.Failure -> {
+                when (response) {
+                    is ApiResponse.Failure.Error -> Result.failure(Exception(response.message()))
+                    is ApiResponse.Failure.Exception -> Result.failure(response.throwable)
+                }
+            }
+        }
+    }
 }
--- app/src/main/kotlin/com/dnd/safety/presentation/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/com/dnd/safety/presentation/ui/login/LoginViewModel.kt
@@ -2,7 +2,9 @@ package com.dnd.safety.presentation.ui.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dnd.safety.domain.usecase.GoogleSignInUseCase
 import com.dnd.safety.domain.usecase.KakaoLoginUseCase
+import com.dnd.safety.presentation.navigation.utils.GoogleSignInHelper
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -10,11 +12,14 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import java.net.SocketTimeoutException
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val kakaoLoginUseCase: KakaoLoginUseCase,
+    private val googleSignInUseCase: GoogleSignInUseCase,
+    private val googleSignInHelper: GoogleSignInHelper,
 ) : ViewModel() {
     private val _state = MutableStateFlow(LoginState())
     val state = _state.asStateFlow()
@@ -36,5 +41,43 @@ class LoginViewModel @Inject constructor(
             }
         )
     }
+    fun onLoginEvent(event: LoginEvent) = viewModelScope.launch {
+        when (event) {
+            is LoginEvent.GoogleSignInSuccess -> loginWithGoogle(event.idToken)
+            is LoginEvent.GoogleSignInError -> handleError(event.error)
+        }
+    }
 
+    private suspend fun loginWithGoogle(idToken: String) {
+        _state.update { it.copy(isLoading = true) }
+
+        googleSignInUseCase(idToken).fold(
+            onSuccess = { token ->
+                _state.update { it.copy(isLoading = false, token = token) }
+                _effect.send(LoginEffect.NavigateToNickName)
+            },
+            onFailure = { throwable ->
+                _state.update { it.copy(isLoading = false) }
+                when(throwable) {
+                    is SocketTimeoutException -> {
+                        _effect.send(LoginEffect.ShowToast("네트워크 연결이 불안정합니다"))
+                    }
+                    else -> {
+                        _effect.send(LoginEffect.ShowToast("일시적인 오류가 발생했습니다"))
+                    }
+                }
+            }
+        )
+    }
+
+    private suspend fun handleError(throwable: Throwable) {
+        _state.update { it.copy(isLoading = false) }
+        _effect.send(LoginEffect.ShowToast(throwable.message ?: "로그인 실패"))
+    }
+
+}
+
+sealed class LoginEvent {
+    data class GoogleSignInSuccess(val idToken: String) : LoginEvent()
+    data class GoogleSignInError(val error: Throwable) : LoginEvent()
 }
