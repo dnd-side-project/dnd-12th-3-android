36241b4c296aa6cf444bb518e4f5dd9c5381bf0c
jhbae12346 <jhbae@logishm.com>
2025-02-15 15:31:39 +0900

    fix: 바텀 네비게이션 디자인 변경

--- app/src/main/kotlin/com/dnd/safety/presentation/navigation/component/MainBottomBar.kt
+++ app/src/main/kotlin/com/dnd/safety/presentation/navigation/component/MainBottomBar.kt
@@ -6,7 +6,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,12 +15,13 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -29,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import com.dnd.safety.presentation.designsystem.theme.Gray30
 import com.dnd.safety.presentation.designsystem.theme.SafetyTheme
 import com.dnd.safety.presentation.navigation.MainTab
 
@@ -45,31 +46,29 @@ internal fun MainBottomBar(
         enter = fadeIn() + slideIn { IntOffset(0, it.height) },
         exit = fadeOut() + slideOut { IntOffset(0, it.height) }
     ) {
-        Column(
-            modifier
+        Surface(
+            shadowElevation = 8.dp,
+            modifier = modifier
         ) {
-            Row(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .height(56.dp)
-                    .border(
-                        width = 1.dp,
-                        color = MaterialTheme.colorScheme.outline,
-                        shape = RoundedCornerShape(size = 28.dp),
-                    )
-                    .background(
-                        color = MaterialTheme.colorScheme.surface,
-                        shape = RoundedCornerShape(28.dp),
-                    )
-                    .padding(horizontal = 28.dp),
-                horizontalArrangement = Arrangement.spacedBy(8.dp),
-            ) {
-                bottomItems.forEach { tab ->
-                    MainBottomBarItem(
-                        tab = tab,
-                        selected = tab == currentItem,
-                        onClick = { onBottomItemClicked(tab) },
-                    )
+            Column {
+                HorizontalDivider()
+                Row(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .height(46.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.surface,
+                            shape = RoundedCornerShape(28.dp),
+                        ),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    bottomItems.forEach { tab ->
+                        MainBottomBarItem(
+                            tab = tab,
+                            selected = tab == currentItem,
+                            onClick = { onBottomItemClicked(tab) },
+                        )
+                    }
                 }
             }
         }
@@ -100,7 +99,8 @@ private fun RowScope.MainBottomBarItem(
         Icon(
             imageVector = tab.icon,
             contentDescription = tab.name,
-            modifier = Modifier.size(34.dp),
+            tint = if (selected) MaterialTheme.colorScheme.primary else Gray30,
+            modifier = Modifier.size(28.dp),
         )
     }
 }
